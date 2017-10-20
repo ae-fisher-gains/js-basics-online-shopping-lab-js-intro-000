cc02cf10302f93f54cac2c25094b4329bf460401
Adam Fisher <ae.fisher.gains@gmail.com>
2017-10-20 12:49:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ function placeOrder(cardNumber) {
   if (cardNumber == undefined) {
     console.log("Sorry, we don't have a credit card on file for you.")
   } else {
-    totalCost = total()
+    var totalCost = total()
     console.log(`Your total cost is ${totalCost}, which will be charged to the card ${cardNumber}.`)
   }
 }
